b18d275ce19edaa1b913f320ce5c1b786cd7d579
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-08-23 14:48:21 +0800

    ADD:add trie interface definition

---
+++ db/trie.go
@@ -0,0 +1,11 @@
+package db
+
+import "github.com/invin/kkchain/common"
+
+type Trie interface {
+	Hash() common.Hash
+	TryGet(key []byte) ([]byte, error)
+	TryUpdate(key, value []byte) error
+	TryDelete(key []byte) error
+	Commit() (common.Hash, error)
+}
