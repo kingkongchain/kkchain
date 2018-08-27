3629b1cfa4bed51d5fabd19603021bba241024e2
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-08-27 14:09:26 +0800

    MOD:add some chain msg code and handle function

--- p2p/chain/handler.go
+++ p2p/chain/handler.go
@@ -11,14 +11,104 @@ type chainHandler func(context.Context, p2p.ID, *Message) (*Message, error)
 
 func (c *Chain) handlerForMsgType(t Message_Type) chainHandler {
 	switch t {
-	case Message_GET_BLOCK:
-		return c.handleGetBlock
+	case Message_CHAIN_STATUS:
+		return c.handleChainStatus
+	case Message_GET_BLOCK_BODIES:
+		return c.handleGetBlockBodies
+	case Message_BLOCKS_BODIES:
+		return c.handleBlockBodies
+	case Message_GET_BLOCK_HEADERS:
+		return c.handleGetBlockHeaders
+	case Message_BLOCK_HEADERS:
+		return c.handleBlockHeaders
+	case Message_TRANSACTIONS:
+		return c.handleTransactions
+	case Message_GET_RECEIPTS:
+		return c.handleGetReceipts
+	case Message_RECEIPTS:
+		return c.handleReceipts
+	case Message_NEW_BLOCK_HASHS:
+		return c.handleNewBlockHashs
+	case Message_NEW_BLOCK:
+		return c.handleNewBlock
 	default:
 		return nil
 	}
 }
 
-func (c *Chain) handleGetBlock(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+func (c *Chain) handleChainStatus(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleGetBlockBodies(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleGetBlockHeaders(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleBlockBodies(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleBlockHeaders(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleTransactions(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleGetReceipts(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleReceipts(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleNewBlockHashs(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
+	// Check result and return corresponding code
+	var resp *Message
+
+	// TODO:
+	return resp, nil
+}
+
+func (c *Chain) handleNewBlock(ctx context.Context, p p2p.ID, pmes *Message) (_ *Message, err error) {
 	// Check result and return corresponding code
 	var resp *Message
 
