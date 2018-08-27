c831bf6695c04bbd2f546535718ee9f576025bcf
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-08-27 23:52:24 +0800

    MOD:mod new chain msg func and add test case

--- p2p/chain/message.go
+++ p2p/chain/message.go
@@ -1,11 +1,30 @@
 package chain
 
 // NewMessage creates a new message object
-func NewMessage(typ Message_Type) *Message {
-	m := &Message{
-		Type: typ,
+func NewMessage(typ Message_Type, data interface{}) *Message {
+	var m *Message
+	switch data.(type) {
+	case *ChainStatusMsg:
+		m = &Message{
+			Type:           typ,
+			ChainStatusMsg: data.(*ChainStatusMsg),
+		}
+		break
+	case *DataMsg:
+		m = &Message{
+			Type:    typ,
+			DataMsg: data.(*DataMsg),
+		}
+		break
+	case *GetBlockHeadersMsg:
+		m = &Message{
+			Type:               typ,
+			GetBlockHeadersMsg: data.(*GetBlockHeadersMsg),
+		}
+		break
+	default:
+		return nil
 	}
-
 	return m
 }
 
