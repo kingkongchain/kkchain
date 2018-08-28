46f93b51878855b537dfdd8b56566c02508a0418
Hsiung <vjoke@foxmail.com>
2018-08-28 10:51:29 +0800

    Add auxiliary files to package common

--- common/hash.go
+++ common/hash.go
@@ -1,5 +1,5 @@
 package common
 
-type Hash [32]byte
+// type Hash [32]byte
 
-type Address [20]byte
+// type Address [20]byte
--- common/math/big_test.go
+++ common/math/big_test.go
@@ -7,7 +7,7 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/invin/kkchain/common"
 )
 
 func TestHexOrDecimal256(t *testing.T) {
