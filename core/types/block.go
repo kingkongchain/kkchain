b6616b99cf7dd5663bb1f6e0681d82168b3a1ad7
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-08-29 17:26:11 +0800

    MOD:adjust block definition

--- core/types/block.go
+++ core/types/block.go
@@ -2,11 +2,20 @@ package types
 
 import (
 	"encoding/binary"
+	"io"
 	"math/big"
+	"sort"
 	"sync/atomic"
 	"time"
+	"unsafe"
 
 	"github.com/invin/kkchain/common"
+	"github.com/invin/kkchain/crypto/sha3"
+	"github.com/invin/kkchain/rlp"
+)
+
+var (
+	EmptyRootHash = DeriveSha(Transactions{})
 )
 
 // nonce for POW computation
@@ -30,8 +39,11 @@ type Header struct {
 	StateRoot   common.Hash    `json:"stateRoot"        gencodec:"required"`
 	TxRoot      common.Hash    `json:"transactionsRoot" gencodec:"required"`
 	ReceiptRoot common.Hash    `json:"receiptsRoot"     gencodec:"required"`
+	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
 	Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
 	Number      *big.Int       `json:"number"           gencodec:"required"`
+	GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
+	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
 	Time        *big.Int       `json:"timestamp"        gencodec:"required"`
 	Extra       []byte         `json:"extraData"        gencodec:"required"`
 	MixDigest   common.Hash    `json:"mixHash"          gencodec:"required"`
@@ -40,14 +52,38 @@ type Header struct {
 
 // Hash returns the block hash of the header
 func (h *Header) Hash() common.Hash {
-	// TODO:
-	return common.Hash{}
+	return rlpHash(h)
 }
 
 // HashNoNonce returns the hash which is used as input for the proof-of-work search.
 func (h *Header) HashNoNonce() common.Hash {
-	//TODO: implement
-	return common.Hash{}
+	return rlpHash([]interface{}{
+		h.ParentHash,
+		h.Miner,
+		h.StateRoot,
+		h.TxRoot,
+		h.ReceiptRoot,
+		h.Bloom,
+		h.Difficulty,
+		h.Number,
+		h.GasLimit,
+		h.GasUsed,
+		h.Time,
+		h.Extra,
+	})
+}
+
+// Size returns the approximate memory used by all internal contents. It is used
+// to approximate and limit the memory consumption of various caches.
+func (h *Header) Size() common.StorageSize {
+	return common.StorageSize(unsafe.Sizeof(*h)) + common.StorageSize(len(h.Extra)+(h.Difficulty.BitLen()+h.Number.BitLen()+h.Time.BitLen())/8)
+}
+
+func rlpHash(x interface{}) (h common.Hash) {
+	hw := sha3.NewKeccak256()
+	rlp.Encode(hw, x)
+	hw.Sum(h[:0])
+	return h
 }
 
 type Block struct {
@@ -68,9 +104,39 @@ type Block struct {
 	ReceivedFrom interface{}
 }
 
-func NewBlock(header *Header, txs []*Transaction) *Block {
-	// TODO:
+type StorageBlock Block
+
+// "external" block encoding. used for protocol, etc.
+type extblock struct {
+	Header *Header
+	Txs    []*Transaction
+}
+
+// "storage" block encoding. used for database.
+type storageblock struct {
+	Header *Header
+	Txs    []*Transaction
+	TD     *big.Int
+}
+
+func NewBlock(header *Header, txs []*Transaction, receipts []*Receipt) *Block {
 	b := &Block{header: CopyHeader(header), td: new(big.Int)}
+
+	// TODO: panic if len(txs) != len(receipts)
+	if len(txs) == 0 {
+		b.header.TxRoot = EmptyRootHash
+	} else {
+		b.header.TxRoot = DeriveSha(Transactions(txs))
+		b.transactions = make(Transactions, len(txs))
+		copy(b.transactions, txs)
+	}
+
+	if len(receipts) == 0 {
+		b.header.ReceiptRoot = EmptyRootHash
+	} else {
+		b.header.ReceiptRoot = DeriveSha(Receipts(receipts))
+		b.header.Bloom = CreateBloom(receipts)
+	}
 
 	return b
 }
@@ -97,21 +163,62 @@ func CopyHeader(h *Header) *Header {
 	return &cpy
 }
 
+// DeprecatedTd is an old relic for extracting the TD of a block. It is in the
+// code solely to facilitate upgrading the database from the old format to the
+// new, after which it should be deleted. Do not use!
+func (b *Block) DeprecatedTd() *big.Int {
+	return b.td
+}
+
+func (b *Block) DecodeRLP(s *rlp.Stream) error {
+	var eb extblock
+	_, size, _ := s.Kind()
+	if err := s.Decode(&eb); err != nil {
+		return err
+	}
+	b.header, b.transactions = eb.Header, eb.Txs
+	b.size.Store(common.StorageSize(rlp.ListSize(size)))
+	return nil
+}
+
+// EncodeRLP serializes b into RLP block format.
+func (b *Block) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, extblock{
+		Header: b.header,
+		Txs:    b.transactions,
+	})
+}
+
+func (b *StorageBlock) DecodeRLP(s *rlp.Stream) error {
+	var sb storageblock
+	if err := s.Decode(&sb); err != nil {
+		return err
+	}
+	b.header, b.transactions, b.td = sb.Header, sb.Txs, sb.TD
+	return nil
+}
+
 func (b *Block) Transactions() Transactions { return b.transactions }
 
-// get tx with given tx hash
-func (b *Block) GetTransaction(hash common.Hash) *Transaction {
-	// TODO:
+func (b *Block) Transaction(hash common.Hash) *Transaction {
+	for _, transaction := range b.transactions {
+		if transaction.Hash() == hash {
+			return transaction
+		}
+	}
 	return nil
 }
 
 func (b *Block) Number() *big.Int     { return new(big.Int).Set(b.header.Number) }
+func (b *Block) GasLimit() uint64     { return b.header.GasLimit }
+func (b *Block) GasUsed() uint64      { return b.header.GasUsed }
 func (b *Block) Difficulty() *big.Int { return new(big.Int).Set(b.header.Difficulty) }
 func (b *Block) Time() *big.Int       { return new(big.Int).Set(b.header.Time) }
 
 func (b *Block) NumberU64() uint64        { return b.header.Number.Uint64() }
 func (b *Block) MixDigest() common.Hash   { return b.header.MixDigest }
 func (b *Block) Nonce() uint64            { return binary.BigEndian.Uint64(b.header.Nonce[:]) }
+func (b *Block) Bloom() Bloom             { return b.header.Bloom }
 func (b *Block) Miner() common.Address    { return b.header.Miner }
 func (b *Block) StateRoot() common.Hash   { return b.header.StateRoot }
 func (b *Block) ParentHash() common.Hash  { return b.header.ParentHash }
@@ -121,12 +228,45 @@ func (b *Block) Extra() []byte            { return common.CopyBytes(b.header.Ext
 
 func (b *Block) Header() *Header { return CopyHeader(b.header) }
 
+func (b *Block) HashNoNonce() common.Hash {
+	return b.header.HashNoNonce()
+}
+
+// Size returns the true RLP encoded storage size of the block, either by encoding
+// and returning it, or returning a previsouly cached value.
+func (b *Block) Size() common.StorageSize {
+	if size := b.size.Load(); size != nil {
+		return size.(common.StorageSize)
+	}
+	c := writeCounter(0)
+	rlp.Encode(&c, b)
+	b.size.Store(common.StorageSize(c))
+	return common.StorageSize(c)
+}
+
+type writeCounter common.StorageSize
+
+func (c *writeCounter) Write(b []byte) (int, error) {
+	*c += writeCounter(len(b))
+	return len(b), nil
+}
+
 func (b *Block) WithSeal(header *Header) *Block {
 	cpy := *header
 	return &Block{
 		header:       &cpy,
 		transactions: b.transactions,
 	}
+}
+
+// WithBody returns a new block with the given transaction and uncle contents.
+func (b *Block) WithBody(transactions []*Transaction, uncles []*Header) *Block {
+	block := &Block{
+		header:       CopyHeader(b.header),
+		transactions: make([]*Transaction, len(transactions)),
+	}
+	copy(block.transactions, transactions)
+	return block
 }
 
 // returns the hash of block header.
@@ -138,3 +278,28 @@ func (b *Block) Hash() common.Hash {
 	b.hash.Store(v)
 	return v
 }
+
+type Blocks []*Block
+
+type BlockBy func(b1, b2 *Block) bool
+
+func (self BlockBy) Sort(blocks Blocks) {
+	bs := blockSorter{
+		blocks: blocks,
+		by:     self,
+	}
+	sort.Sort(bs)
+}
+
+type blockSorter struct {
+	blocks Blocks
+	by     func(b1, b2 *Block) bool
+}
+
+func (self blockSorter) Len() int { return len(self.blocks) }
+func (self blockSorter) Swap(i, j int) {
+	self.blocks[i], self.blocks[j] = self.blocks[j], self.blocks[i]
+}
+func (self blockSorter) Less(i, j int) bool { return self.by(self.blocks[i], self.blocks[j]) }
+
+func Number(b1, b2 *Block) bool { return b1.header.Number.Cmp(b2.header.Number) < 0 }
