c069ca35a70000fd1cd94cbf1e565d6453e695f3
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-08-29 16:44:25 +0800

    MOD:adjust transaction definition

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -1,10 +1,12 @@
 package types
 
 import (
+	"crypto/ecdsa"
 	"errors"
 	"math/big"
 
 	"github.com/invin/kkchain/common"
+	"github.com/invin/kkchain/crypto"
 )
 
 var (
@@ -29,4 +31,43 @@ type Signer interface {
 
 	// Hash returns the hash to be signed.
 	Hash(tx *Transaction) common.Hash
+
+	// Equal returns true if the given signer is the same as the receiver.
+	Equal(Signer) bool
+}
+
+// SignTx signs the transaction using the given signer and private key
+func SignTx(tx *Transaction, s Signer, prv *ecdsa.PrivateKey) (*Transaction, error) {
+	h := s.Hash(tx)
+	sig, err := crypto.Sign(h[:], prv)
+	if err != nil {
+		return nil, err
+	}
+	return tx.WithSignature(s, sig)
+}
+
+// Sender returns the address derived from the signature (V, R, S) using secp256k1
+// elliptic curve and an error if it failed deriving or upon an incorrect
+// signature.
+//
+// Sender may cache the address, allowing it to be used regardless of
+// signing method. The cache is invalidated if the cached signer does
+// not match the signer used in the current call.
+func Sender(signer Signer, tx *Transaction) (common.Address, error) {
+	if sc := tx.from.Load(); sc != nil {
+		sigCache := sc.(sigCache)
+		// If the signer used to derive from in a previous
+		// call is not the same as used current, invalidate
+		// the cache.
+		if sigCache.signer.Equal(signer) {
+			return sigCache.from, nil
+		}
+	}
+
+	addr, err := signer.Sender(tx)
+	if err != nil {
+		return common.Address{}, err
+	}
+	tx.from.Store(sigCache{signer: signer, from: addr})
+	return addr, nil
 }
