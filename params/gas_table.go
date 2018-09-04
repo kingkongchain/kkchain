d902f55e646232b16ea95980131276aef53070ce
Hsiung <vjoke@foxmail.com>
2018-09-04 15:16:35 +0800

    Add package param for chain configurations

---
+++ params/gas_table.go
@@ -0,0 +1,37 @@
+
+package params
+
+// GasTable organizes gas prices for different ethereum phases.
+type GasTable struct {
+	ExtcodeSize uint64
+	ExtcodeCopy uint64
+	ExtcodeHash uint64
+	Balance     uint64
+	SLoad       uint64
+	Calls       uint64
+	Suicide     uint64
+
+	ExpByte uint64
+
+	// CreateBySuicide occurs when the
+	// refunded account is one that does
+	// not exist. This logic is similar
+	// to call. May be left nil. Nil means
+	// not charged.
+	CreateBySuicide uint64
+}
+
+// Variables containing gas prices for different ethereum phases.
+var (
+	// GasTableHomestead contain the gas prices for
+	// the homestead phase.
+	GasTableHomestead = GasTable{
+		ExtcodeSize: 20,
+		ExtcodeCopy: 20,
+		Balance:     20,
+		SLoad:       50,
+		Calls:       40,
+		Suicide:     0,
+		ExpByte:     10,
+	}
+)
