b7d3649dc210f7c6fc78b5c2557405fcaa14bd31
zj-db1056 <zj-db1056@zj-db1056deMacBook-Pro.local>
2018-10-15 14:09:16 +0800

    ADD:add accounts and api base

---
+++ params/denomination.go
@@ -0,0 +1,16 @@
+package params
+
+// These are the multipliers for ether denominations.
+// Example: To get the wei value of an amount in 'douglas', use
+//
+//    new(big.Int).Mul(value, big.NewInt(params.Douglas))
+//
+const (
+	Wei     = 1
+	Ada     = 1e3
+	Babbage = 1e6
+	Shannon = 1e9
+	Szabo   = 1e12
+	Finney  = 1e15
+	King    = 1e18
+)
